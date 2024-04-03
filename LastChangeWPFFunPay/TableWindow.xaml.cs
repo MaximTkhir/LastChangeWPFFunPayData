b89eae27c9061d66077621fe1b127c9c737835ae
PROSTOMAX <tmaximus27122005@gmail.com>
2024-04-03 13:09:26 +0300

    Add project files.

---
+++ LastChangeWPFFunPay/TableWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using LastChangeWPFFunPay.LastChangeFourPrgFunPayDataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LastChangeWPFFunPay
+{
+    /// <summary>
+    /// Interaction logic for TableWindow.xaml
+    /// </summary>
+    public partial class TableWindow : Window
+    {
+        LastChangeBaseViewDataTableAdapter lastfun = new LastChangeBaseViewDataTableAdapter();
+        public TableWindow()
+        {
+            InitializeComponent();
+            AllInfa.ItemsSource = lastfun.GetData();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+    }
+}
